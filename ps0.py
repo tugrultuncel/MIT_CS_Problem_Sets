5304b71c33c6c006432837e10bfb2a299586edaf
tugrul tuncel <tugrultuncel95@gmail.com>
2026-01-18 12:13:54 +0100

    Create ps0.py
    #solution for Problem set 1

---
+++ ps0.py
@@ -0,0 +1,25 @@
+#Problem Set 0
+#Name: Tugrul Tuncel
+#Collaborators:
+#Time Spent: 0:30
+# -*- coding: utf-8 -*-
+#this file makes sure that user installed spyder and python correctly
+
+"""
+Created on Sun Jan 18 12:03:39 2026
+
+@author: tugru
+"""
+
+
+import numpy
+
+x=5
+y=8
+
+z=x+y
+
+a=numpy.log2(z)
+
+print a 
+
